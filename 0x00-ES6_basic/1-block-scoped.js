dc4a8ec4d788b215647c587a69a1b8cf9b645a65
Alexoat76 <3915@holbertonschool.com>
2022-07-25 13:15:39 -0500

    The variables aren’t overwritten inside the conditional block | :heavy_check_mark:

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+export default function taskBlock(trueOrFalse) { // create a function that returns true or false
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+      // eslint-disable-next-line
+      const task = true;
+      // eslint-disable-next-line
+      const task2 = false;
+    }
+  
+    return [task, task2]; // return an array of the two tasks
+  }
+  
