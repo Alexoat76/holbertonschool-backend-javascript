51d2ad43dbb6f36692f3329128df7bb3757369f3
Alexoat76 <3915@holbertonschool.com>
2022-07-27 10:50:51 -0500

    Import 2 previous functions | Accept 3 Args | All promises settled | Return Array structure :heavy_check_mark:

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+// Handle multiple promises
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+}
+
+export default handleProfileSignup;
+// export default is the same as module.exports = handleProfileSignup;
