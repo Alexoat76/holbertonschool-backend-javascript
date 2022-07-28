9a27ffcfd994e4b5dd3d1f822800dd2713efd97d
Alexoat76 <3915@holbertonschool.com>
2022-07-28 18:57:03 -0500

    Code fixed and working :heavy_check_mark:

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -34,7 +33,6 @@ class StudentHolberton {
   }
 }
 
-
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -44,6 +42,6 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-const listOfStudents = [student1, student2, student3, student4, student5]; // create an array of students
+const listOfStudents = [student1, student2, student3, student4, student5];
 
-export default listOfStudents// export the array of students
+export default listOfStudents;
