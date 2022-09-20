3ae87c06614d77cc37e3f92ac5c711209b1ab8b6
Alexoat76 <3915@holbertonschool.com>
2022-09-20 07:39:32 -0500

    Create program that will be executed | command line | :heavy_check_mark:

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,14 +4,14 @@ process.stdin.setEncoding('utf-8'); // set encoding to utf-8
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read(); // read input from stdin
-  if (name) process.stdout.write(`Your name is: ${name}\n`);
+  if (name) process.stdout.write(`Your name is: ${name}`);
 });
 /**
  * If stdout process in stream comes from the terminal, end the process
  * with a message
  */
 if (process.stdout.isTTY) {
-  process.stdin.on('end', () => {
+  process.stdin.on('close', () => {
     process.stdout.write('This important software is now closing\n');
   });
 }
